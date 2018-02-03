853995daeec9ce9d059d58ced89a94283d02dbc5
irakli97 <ikoberidze97@gmail.com>
2018-02-03 12:10:18 +0400

    Update Implementation.py

--- Implementation.py
+++ Implementation.py
@@ -1,23 +1,24 @@
-import numpy
+import numpy as np
 from sklearn import preprocessing
 import pandas as pd
-from keras.datasets import mnist
+from keras.optimizers import Adam
 from keras.models import Sequential
-from keras.layers import Dense, Input
+from keras.layers import Dense
 from keras.layers import AlphaDropout
-from keras.layers import Flatten
-from keras.utils import np_utils, to_categorical
+from keras.utils  import to_categorical
 from keras import backend as K
-from keras.preprocessing.image import ImageDataGenerator
+
 K.set_image_dim_ordering('th')
-numpy.random.seed(7) 
-my_data = numpy.array(pd.read_csv('HTRU_2.csv', sep=',',header=None))
-x = my_data[:,[0,1,2,3,4,5,6,7]]
-y=my_data[:,8]
+np.random.seed(7) 
+my_data = np.array(pd.read_csv('HTRU_2.csv', sep=',',header=None))
+
+x = my_data[:,0:8]
+y = my_data[:,8]
 
 y = to_categorical(y)
 num_classes = 2
 x = preprocessing.scale(x)
+#x = (x - np.mean(x)) / np.std(x) 
 
 model = Sequential()
 '''
@@ -28,33 +29,32 @@ Intent:
     inputs normalized to zero mean and unit variance   
     
 '''
-model.add(Dense(512,activation='selu',kernel_initializer='lecun_normal', input_shape=(8,)))
+model.add(Dense(512, activation='selu', kernel_initializer='lecun_normal', input_shape=(8,)))
 model.add(AlphaDropout(0.05))
 model.add(Dense(256, activation='selu', kernel_initializer='lecun_normal'))
 model.add(AlphaDropout(0.05))
 model.add(Dense(128, activation='selu', kernel_initializer='lecun_normal'))
 model.add(AlphaDropout(0.05))
-model.add(Dense(64, activation='selu', kernel_initializer='lecun_normal'))
+model.add(Dense(64, activation='selu',  kernel_initializer='lecun_normal'))
 model.add(AlphaDropout(0.05))
-model.add(Dense(32, activation='selu', kernel_initializer='lecun_normal'))
+model.add(Dense(32, activation='selu',  kernel_initializer='lecun_normal'))
 model.add(AlphaDropout(0.05))
-model.add(Dense(16, activation='selu', kernel_initializer='lecun_normal'))
+model.add(Dense(16, activation='selu',  kernel_initializer='lecun_normal'))
 model.add(AlphaDropout(0.05))
-model.add(Dense(8, activation='selu', kernel_initializer='lecun_normal', bias_initializer='lecun_normal'))
+model.add(Dense(8, activation='selu',   kernel_initializer='lecun_normal'))
 model.add(AlphaDropout(0.05))
-model.add(Dense(4, activation='selu', kernel_initializer='lecun_normal', bias_initializer='lecun_normal'))
+model.add(Dense(4, activation='selu',   kernel_initializer='lecun_normal'))
 model.add(AlphaDropout(0.05))
-model.add(Dense(2, activation='selu', kernel_initializer='lecun_normal', bias_initializer='lecun_normal'))
-model.add(AlphaDropout(0.05))
+
 
 #output Layer
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(2, activation='softmax'))
 # Compile model
-model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.99, epsilon=0.01, decay=0.0)
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-model.fit(x, y, validation_split=0.3, epochs=100, batch_size=100, verbose=1, shuffle=True)
+model.fit(x, y, validation_split=0.3, epochs=10, batch_size=100, verbose=1, shuffle=True)
 # Final evaluation of the model
 scores = model.evaluate(x, y, verbose=0)
 print("Error: %.2f%%" % (100-scores[1]*100))
 # Error: 1.89%
-
